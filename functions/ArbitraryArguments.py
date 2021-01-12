3548a015c69692215d83124447eaabcc0440eb28
JesusSePe <umbreon20019@gmail.com>
2021-01-12 13:16:41 +0100

    Lambda explained

--- functions/ArbitraryArguments.py
+++ functions/ArbitraryArguments.py
@@ -6,4 +6,22 @@ def sum(a, *b):
         result += element
     return result
 
+
 print(sum(5,5,5,5,5,6))
+
+
+# LAMBDA EXPLANATION:
+# Lambda works as an anonymous function, and is used for simple operations.
+# In this example, we have a function (myfunc) that requires an argument (n), and everything it does is return
+# the lambda function a : a * n.
+# The functions mydoubler and mytripler calls myfunc, giving the arguments 2 and 3.
+# What it does, is change the n value with 2 or 3, and when those functions are called, the given number (11) is a
+def myfunc(n):
+    return lambda a : a * n
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+
