cee4599881d808219e88b5227c8b1f7ff4288a02
JesusSePe <umbreon20019@gmail.com>
2021-02-01 12:35:19 +0100

    Basic recursivity. Add numbers and return result.

--- recursivity/additions.py
+++ recursivity/additions.py
@@ -1,6 +1,8 @@
 def sum(nums):
+    # once there are no more numbers on the list, returns the last number.
     if len(nums) == 1:
         return nums[0]
+    # The last item on list + all the other items.
     else:
         return nums[-1] + sum(nums[:-1])
 
