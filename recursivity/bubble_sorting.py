62a2655d7022c4451a9ef04c65a80b3e9710eda5
JesusSePe <umbreon20019@gmail.com>
2021-02-13 19:50:10 +0100

    bubble sort vertically completed.

--- recursivity/bubble_sorting.py
+++ recursivity/bubble_sorting.py
@@ -28,3 +28,22 @@ def recursive_bubble_v2(list, iterations, comparisons):
 
 ul2 = [[3, 1, 2], [8, 4, 5], [9, -1, 2]]
 print(recursive_bubble_v2(ul2, 0, 0))
+
+
+# Recursive bubble sort for columns (Why? I don't know, this is homework.)
+
+
+def recursive_bubble_v3(list, iterations, comparisons):
+    if iterations > len(list)-2:
+        return list
+    if comparisons > len(list[iterations])-1:
+        return recursive_bubble_v3(list, iterations+1, 0)
+    if list[iterations][comparisons] > list[iterations+1][comparisons]:
+        list[iterations][comparisons], list[iterations+1][comparisons] = list[iterations + 1][comparisons], list[iterations][comparisons]
+    if list[iterations-1][comparisons] > list[iterations][comparisons]:
+        list[iterations-1][comparisons], list[iterations][comparisons] = list[iterations][comparisons], list[iterations-1][comparisons]
+    return recursive_bubble_v3(list, iterations, comparisons + 1)
+
+
+ul3 = [[99, 2, -1], [2, 15, -2], [1, 5, -7]]
+print(recursive_bubble_v3(ul3, 0, 0))
